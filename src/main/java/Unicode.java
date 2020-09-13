bcb38e8d8459285eda6d2c9d4ad0b06a9a555101
fredoverflow <fredoverflow@users.noreply.github.com>
2020-09-13 14:25:27 +0200

    if UTF-8 check already detects binary, skip pointless Latin-1 check

--- src/main/java/Unicode.java
+++ src/main/java/Unicode.java
@@ -2,42 +2,55 @@ import java.nio.charset.StandardCharsets;
 
 public class Unicode {
     public static String decodeHeuristically(byte[] bytes, int start) {
-        String result = tryUtf8(bytes, start);
-        if (result == null) {
-            result = tryLatin1(bytes, start);
-            if (result == null) {
-                result = hexdump(bytes, start);
-            }
-        }
-        return result;
+        // https://en.wikipedia.org/wiki/UTF-8#Encoding
+        // https://en.wikipedia.org/wiki/ISO/IEC_8859-1#Code_page_layout
+        return utf8_latin1_binary(bytes, start);
+        // TODO support UTF-16 https://en.wikipedia.org/wiki/Byte_order_mark
     }
 
-    private static String tryUtf8(byte[] bytes, int start) {
+    private static String utf8_latin1_binary(byte[] bytes, int start) {
         for (int i = start; i < bytes.length; ++i) {
             int b = bytes[i] & 255;
             switch (b >>> 3) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    if (((RETURN_NEWLINE_TAB >>> b) & 1) == 0) return null;
-                    break;
-                // https://en.wikipedia.org/wiki/UTF-8#Encoding
+                case 0b00001:
+                    if (((RETURN_NEWLINE_TAB >>> b) & 1) != 0) continue;
+                    // intentional case fallthrough
+                case 0b00000:
+                case 0b00010:
+                case 0b00011:
+
+                case 0b10000:
+                case 0b10001:
+                case 0b10010:
+                case 0b10011:
+                    return hexdump(bytes, start);
+
+                case 0b10100:
+                case 0b10101:
+                case 0b10110:
+                case 0b10111:
+
+                case 0b11111:
+                    return latin1_binary(bytes, start);
+
                 case 0b11110:
                     // quadruplets
-                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000) return null;
+                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000)
+                        return latin1_binary(bytes, start);
                     // intentional case fallthrough
                 case 0b11100:
                 case 0b11101:
                     // triplets
-                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000) return null;
+                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000)
+                        return latin1_binary(bytes, start);
                     // intentional case fallthrough
                 case 0b11000:
                 case 0b11001:
                 case 0b11010:
                 case 0b11011:
                     // pairs
-                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000) return null;
+                    if (i + 1 >= bytes.length || (bytes[++i] & 0b11000000) != 0b10000000)
+                        return latin1_binary(bytes, start);
             }
         }
         return new String(bytes, start, bytes.length - start, StandardCharsets.UTF_8);
@@ -45,10 +58,10 @@ public class Unicode {
 
     private static final int RETURN_NEWLINE_TAB = 1 << '\r' | 1 << '\n' | 1 << '\t';
 
-    private static String tryLatin1(byte[] bytes, int start) {
+    private static String latin1_binary(byte[] bytes, int start) {
         for (int i = start; i < bytes.length; ++i) {
             if (!isValidCharacter(bytes[i] & 255, LATIN1)) {
-                return null;
+                return hexdump(bytes, start);
             }
         }
         return new String(bytes, start, bytes.length - start, StandardCharsets.ISO_8859_1);
@@ -60,7 +73,6 @@ public class Unicode {
         return ((valid[outer] >>> inner) & 1) != 0;
     }
 
-    // https://en.wikipedia.org/wiki/ISO/IEC_8859-1#Code_page_layout
     private static final int[] LATIN1 = {RETURN_NEWLINE_TAB, -1, -1, 0x7fffffff, 0, -1, -1, -1};
 
     private static String hexdump(byte[] bytes, int start) {
