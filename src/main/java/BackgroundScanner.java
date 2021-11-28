71e625a1433eb546fbd7d184c97b073ecfb86c61
fredoverflow <fredoverflow@users.noreply.github.com>
2021-11-28 12:30:33 +0100

    filter blobs by payload text

--- src/main/java/BackgroundScanner.java
+++ src/main/java/BackgroundScanner.java
@@ -3,12 +3,13 @@ import java.awt.*;
 import java.io.File;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BackgroundScanner {
     private static final Font FONT_PROGRESS = new Font("SansSerif", Font.PLAIN, 48);
 
-    public static void scan(File selectedDirectory, Consumer<GitBlob[]> processGitBlobs) {
+    public static void scan(File selectedDirectory, Consumer<List<GitBlob>> processGitBlobs) {
         JDialog dialog = new JDialog((JDialog) null, "Scanning...", false);
 
         JProgressBar progressBar = new JProgressBar();
@@ -22,13 +23,13 @@ public class BackgroundScanner {
         dialog.setLocationRelativeTo(null); // center
         dialog.setVisible(true);
 
-        new SwingWorker<GitBlob[], GitBlob>() {
+        new SwingWorker<List<GitBlob>, GitBlob>() {
             private int countGitBlobs;
 
             @Override
-            protected GitBlob[] doInBackground() throws Exception {
+            protected List<GitBlob> doInBackground() throws Exception {
                 try (Stream<GitBlob> gitBlobs = GitBlob.findGitBlobs(selectedDirectory.toPath(), this::publish)) {
-                    return gitBlobs.toArray(GitBlob[]::new);
+                    return gitBlobs.collect(Collectors.toList());
                 }
             }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import static javax.swing.ScrollPaneConstants.*;
 
@@ -13,11 +18,11 @@ public class Main {
     private static final int ROWS = 25;
     private static final int COLUMNS = 80;
 
-    private static final Font FONT_DATE = new Font("SansSerif", Font.PLAIN, 20);
+    private static final Font FONT_EXPLORER = new Font("SansSerif", Font.PLAIN, 20);
     private static final Font FONT_PAYLOAD = new Font("Monospaced", Font.PLAIN, 24);
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(Main::selectDirectory);
+        EventQueue.invokeLater(Main::selectDirectory);
     }
 
     private static void selectDirectory() {
@@ -29,44 +34,155 @@ public class Main {
         }
     }
 
-    private static void createUi(GitBlob[] listData) {
+    private static void createUi(List<GitBlob> allGitBlobs) {
         JFrame frame = new JFrame("Pangit");
+        JPanel explorerPanel = new JPanel(new BorderLayout());
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.Y_AXIS));
 
-        JList<GitBlob> gitBlobs = new JList<>(listData);
-        gitBlobs.setFont(FONT_DATE);
-        frame.add(new JScrollPane(gitBlobs, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.WEST);
+        JTextField searchField = new JTextField();
+        searchField.setFont(FONT_EXPLORER);
+        searchPanel.add(searchField);
 
-        JTextArea payload = new JTextArea(GETTING_STARTED, ROWS, COLUMNS);
-        payload.setFont(FONT_PAYLOAD);
-        payload.setEditable(false);
-        DefaultCaret caret = (DefaultCaret) payload.getCaret();
+        JPanel searchOptions = new JPanel();
+        JCheckBox optionCaseSensitive = new JCheckBox("case sensitive");
+        optionCaseSensitive.setFont(FONT_EXPLORER);
+        searchOptions.add(optionCaseSensitive);
+        optionCaseSensitive.addActionListener(event -> {
+            searchField.requestFocusInWindow();
+        });
+
+        JCheckBox optionRegex = new JCheckBox("regex");
+        optionRegex.setFont(FONT_EXPLORER);
+        searchOptions.add(optionRegex);
+        optionRegex.addActionListener(event -> {
+            searchField.requestFocusInWindow();
+        });
+        searchPanel.add(searchOptions);
+        explorerPanel.add(searchPanel, BorderLayout.NORTH);
+
+        DefaultListModel<GitBlob> filteredGitBlobsModel = new DefaultListModel<>();
+        // filteredGitBlobsModel.addAll(allGitBlobs);
+        allGitBlobs.forEach(filteredGitBlobsModel::addElement);
+        JList<GitBlob> filteredGitBlobs = new JList<>(filteredGitBlobsModel);
+        filteredGitBlobs.setFont(FONT_EXPLORER);
+        explorerPanel.add(new JScrollPane(filteredGitBlobs, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
+        frame.add(explorerPanel, BorderLayout.WEST);
+
+        JTextArea payloadArea = new JTextArea(GETTING_STARTED, ROWS, COLUMNS);
+        payloadArea.setFont(FONT_PAYLOAD);
+        payloadArea.setEditable(false);
+        DefaultCaret caret = (DefaultCaret) payloadArea.getCaret();
         caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
-        frame.add(new JScrollPane(payload, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+        frame.add(new JScrollPane(payloadArea, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
 
-        gitBlobs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        gitBlobs.addListSelectionListener(event -> {
-            if (!event.getValueIsAdjusting()) {
-                GitBlob gitBlob = gitBlobs.getSelectedValue();
-                frame.setTitle(gitBlob.path.toString());
-                try {
-                    payload.setText(gitBlob.payload());
-                } catch (IOException ex) {
-                    payload.setText(ex.toString());
+        searchField.addActionListener(new ActionListener() {
+            private String lastSearchText;
+            private int lastSearchFlags;
+
+            private int searchFlags() {
+                int flags = Pattern.UNICODE_CHARACTER_CLASS;
+                if (!optionCaseSensitive.isSelected()) {
+                    flags |= Pattern.CASE_INSENSITIVE;
                 }
+                if (!optionRegex.isSelected()) {
+                    flags |= Pattern.LITERAL;
+                }
+                return flags;
+            }
+
+            private void searchPanelComponentsSetEnabled(boolean enabled) {
+                searchField.setEnabled(enabled);
+                optionCaseSensitive.setEnabled(enabled);
+                optionRegex.setEnabled(enabled);
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String searchText = searchField.getText();
+                int searchFlags = searchFlags();
+                if (searchText.equals(lastSearchText) && searchFlags == lastSearchFlags) {
+                    return;
+                }
+
+                Pattern pattern;
+                try {
+                    pattern = Pattern.compile(searchText, searchFlags);
+                } catch (PatternSyntaxException ex) {
+                    payloadArea.setText(ex.getMessage());
+                    return;
+                }
+
+                lastSearchText = searchText;
+                lastSearchFlags = searchFlags;
+                filteredGitBlobsModel.clear();
+
+                if (searchText.isEmpty()) {
+                    // filteredGitBlobsModel.addAll(allGitBlobs);
+                    allGitBlobs.forEach(filteredGitBlobsModel::addElement);
+                    return;
+                }
+
+                searchPanelComponentsSetEnabled(false);
+
+                new SwingWorker<Void, GitBlob>() {
+                    @Override
+                    protected Void doInBackground() {
+                        for (GitBlob gitBlob : allGitBlobs) {
+                            try {
+                                if (pattern.matcher(gitBlob.payload()).find()) {
+                                    publish(gitBlob);
+                                }
+                            } catch (IOException ex) {
+                                EventQueue.invokeLater(() -> {
+                                    payloadArea.setText(ex.getMessage());
+                                });
+                            }
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void process(List<GitBlob> matchingGitBlobs) {
+                        // filteredGitBlobsModel.addAll(matchingGitBlobs);
+                        matchingGitBlobs.forEach(filteredGitBlobsModel::addElement);
+                    }
+
+                    @Override
+                    protected void done() {
+                        searchPanelComponentsSetEnabled(true);
+                        searchField.requestFocusInWindow();
+                    }
+                }.execute();
             }
         });
 
-        payload.addMouseWheelListener(event -> {
+        filteredGitBlobs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        filteredGitBlobs.addListSelectionListener(event -> {
+            GitBlob gitBlob = filteredGitBlobs.getSelectedValue();
+            if (event.getValueIsAdjusting() || gitBlob == null) {
+                return;
+            }
+
+            frame.setTitle(gitBlob.path.toString());
+            try {
+                payloadArea.setText(gitBlob.payload());
+            } catch (IOException ex) {
+                payloadArea.setText(ex.getMessage());
+            }
+        });
+
+        payloadArea.addMouseWheelListener(event -> {
             if (isControlRespectivelyCommandDown(event)) {
-                Font oldFont = payload.getFont();
+                Font oldFont = payloadArea.getFont();
                 int oldSize = oldFont.getSize();
                 int newSize = oldSize - event.getWheelRotation();
                 if (newSize > 0) {
                     Font newFont = oldFont.deriveFont((float) newSize);
-                    payload.setFont(newFont);
+                    payloadArea.setFont(newFont);
                 }
             } else {
-                payload.getParent().dispatchEvent(event);
+                payloadArea.getParent().dispatchEvent(event);
             }
         });
 
